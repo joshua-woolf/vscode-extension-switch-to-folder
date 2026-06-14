007824b4e03dbdb25769b0b0169898a491c71ab9
Joshua Woolf <joshuawoolf87@gmail.com>
2026-06-14 14:34:30 +0200

    fix ci

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from '@vscode/test-cli';
+import { tmpdir } from 'node:os';
+import { join } from 'node:path';
 
 export default defineConfig({
-	files: 'out/test/**/*.test.js'
+	files: 'out/test/**/*.test.js',
+	launchArgs: [
+		`--user-data-dir=${join(tmpdir(), `switch-to-folder-vscode-test-${process.pid}`)}`
+	],
 });
